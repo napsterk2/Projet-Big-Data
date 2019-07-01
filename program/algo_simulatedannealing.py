ce9123705bf3f490a53729574bed109f50620538
Coconus98 <coconus98@gmail.com>
2019-07-01 15:23:04 +0200

    Fonctionnal simulated annealing, must add constraints

--- program/algo_simulatedannealing.py
+++ program/algo_simulatedannealing.py
@@ -4,11 +4,11 @@ from matplotlib.pyplot import *
 #Definition des paramètres
 
 cities = 50
-T0 = 10.0 #temperature initiale
+T0 = 15.0 #temperature initiale
 Tmin = 0.01 #fixe la fin du recuit
 tau = 10000 #définit la vitesse de baisse de température
 
-#Fonction qui va permettre de calculer l'énergie totale, en mesurant la somme des distances entre les villes dans l'ordre de parcours du trajet
+#Fonction qui va permettre de calculer l'énergie totale, en mesurant la somme des distances entre les villes dans l'ordre de parcours du trajet, c'est à dire donc la distance de trajet.
 def totalEnergy():
     global path
     energy = 0
@@ -18,32 +18,70 @@ def totalEnergy():
 
 #Fonction qui va swapper un certain nombre de segments (entre i et j), ce qui entrainera un changement d'énergie dans le système
 #Swap inverse lorsqu'elle est réappellé avec les mêmes valeurs (pour retour aux trajets antérieurs)
-def swapping():
+def swapping(i, j):
     global path
     Minimum = min(i,j)
     Maximum = max(i,j)
     path[Minimum:Maximum] = path[Minimum:Maximum].copy()[::-1]
     return
 
+#Vérification de l'énergie, et on décide si la fluctuation est gardée ou non selon la proba de l'algo
 def Metropolis(E1,E2):
     global T
     if E1 <= E2:
         E2 = E1  # énergie du nouvel état = énergie système
     else:
         dE = E1-E2
-        if random.uniform() > exp(-dE/T): # la fluctuation est retenue avec  
-            Fluctuation(i,j)              # la proba p. sinon retour trajet antérieur
+        if random.uniform() > exp(-dE/T): # la fluctuation est retenue avec la proba p. sinon retour trajet antérieur
+            swapping(i,j)              
         else:
-            E2 
+            E2 = E1 # la fluctuation est retenue 
+    return E2
+
+#variables à instancer
+t = 0
+T = T0
 
 #variables d'historique
 Henergy = []
 Htime = []
-HTemp = []
+Htemp = []
 
 #placement de N villes sur le plan
 x = random.uniform(size=cities)
 y = random.uniform(size=cities)
 
 #trajet initial réalisé selon l'ordre d'apparition des villes
+path = arange(cities)
+initialPath = path.copy()
+
+#On calcule la distance initiale séparant les villes, qu'il va falloir minimiser
+Ec = totalEnergy()
+
+while T>Tmin:
+    #permet de choisir deux villes différentes au hasard
+    i = random.randint(0, cities-1)
+    j = random.randint(0, cities-1)
+    if i == j: continue
+
+    #On appelle le swapping et on mesure l'énergie
+    swapping(i, j)
+    Ef = totalEnergy()
+    Ec = Metropolis(Ef, Ec)
+
+    #loi de refroidissmenet
+    t += 1
+    T = T0*exp(-t/tau)
+
+    #on rajoute les variables à l'historique
+    if t % 10 == 0:
+        Henergy.append(Ec)
+        Htime.append(t)
+        Htemp.append(T)
+
+#display de nos resultats
+print(float(Henergy[0]))
+print(float(Ec))
+print(float(t))
+
 
